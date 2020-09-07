921f3a1ec7121d71f54dcd4034a27f8eb1e74a63
Peter Nguyen <peter@datacrunch.ca>
2020-09-06 22:04:30 -0400

    Added legend

--- covid-ontario-csv.py
+++ covid-ontario-csv.py
@@ -1,20 +1,20 @@
-import csv
 import urllib.request
+import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
 
 filename = "https://data.ontario.ca/dataset/f4f86e54-872d-43f8-8a86-3892fd3cb5e6/resource/ed270bb8-340b-41f9-a7c6-e8ef587e6d11/download/covidtesting.csv"
 response = urllib.request.urlopen(filename)
 lines = [l.decode("utf-8") for l in response.readlines()]
-
 reader = csv.reader(lines)
 header_row = next(reader)
+
 # print(header_row)
 
 # for index, column_header in enumerate(header_row):
     # print(index, column_header)
 
-# Get dates and high temperatures from this file.
+# Get dates, case and death counts from CSV file.
 dates, cases, deaths = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[0], "%Y-%m-%d")
@@ -35,17 +35,20 @@ for row in reader:
 
 # print(deaths)
 
-# Plot the high temperatures.
-plt.style.use("seaborn")
+# Plot the counts.
+plt.style.use("dark_background")
 fig, ax = plt.subplots()
-ax.scatter(dates, cases, c=cases, cmap=plt.cm.Blues, s=10)
-ax.scatter(dates, deaths, c=deaths, cmap=plt.cm.Reds, s=10)
+ax.scatter(dates, cases, c="blue", s=2, label="Confirmed Cases")
+ax.scatter(dates, deaths, c="red", s=2, label="Cumulative Deaths")
 
 # Format plot.
-plt.title("Ontario COVID-19 Cases and Deaths", fontsize=24)
-plt.xlabel("", fontsize=16)
+plt.title("Ontario COVID-19\nConfirmed Cases and Cumulative Deaths", fontsize=16)
+plt.xlabel("Date", fontsize=12)
 fig.autofmt_xdate()
-plt.ylabel("Count", fontsize=16)
+plt.ylabel("Count", fontsize=12)
 plt.tick_params(axis="both", which="major", labelsize=16)
+
+# Add legend
+ax.legend(loc="upper right", frameon=True)
 
 plt.show()
